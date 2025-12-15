153694bf6b598acd9c5204ffb7186237dc31f1bb
Jregueirava <jesusregueiravazquz@gmail.com>
2025-12-15 19:35:20 +0100

    bici_api, parte del codigo hecha

---
+++ lib/data/bici_api.dart
@@ -0,0 +1,40 @@
+//Se encarga de las peticiones HTTP a la API de BiciCoruña
+
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class BiciApi{
+  //URLS de los endpoints(General Bikeshare Feed Specification)
+  static const String _baseInfo=
+  "https://acoruna.publicbikesystem.net/customer/gbfs/v2/gl/station_information";
+  static const String _baseStatus =
+  "https://acoruna.publicbikesystem.net/customer/gbfs/v2/gl/station_status";
+
+
+//Info estatica de todas las estaciones
+Future<List<dynamic>> getStationsInfoJson() async{
+  final url = Uri.parse(_baseInfo);
+
+  //Peticion asincrona
+  final res = await http.get(url);
+
+  //Validar respuesta HHTP
+
+  if(res.statusCode != 200){
+    throw Exception("Error HTTP ${res.statusCode} en station_information");
+
+  }
+
+  //Decodificaion del JSON
+  final decoded = jsonDecode(res.body);
+
+  if(decoded is! Map || decoded["data"] == null){
+    throw Exception("Formato inesperado en station_information");
+  }
+
+  final data = decoded["data"] as Map<String, dynamic>;
+  final stations = data["stations"] as List;
+  return stations;
+}
+
+}
